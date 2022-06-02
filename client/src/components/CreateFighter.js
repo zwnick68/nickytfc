fcce371cbcd3dfa1c33df0124bd9e07ac652953a
Dmitriy <khaitdmitriy@gmail.com>
2022-06-02 11:44:41 -0400

    added login/signup frontend

---
+++ client/src/components/CreateFighter.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+function CreateFighter ( ) {
+    return (
+        <div>
+   test
+    
+    </div>   
+
+    )
+}
+
+
+
+export default CreateFighter;
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react';
+import Login from './Login';
+import SignUp from './SignUp';
+
 
 function HomePage() {
     
@@ -6,8 +9,10 @@ function HomePage() {
     return(
         <div className='homepageclass'>
              <div class="topdiv"> Elite Fighter Analytics </div>
-        
+            <Login/>
+            <SignUp/> 
             <img className="home-image" src="https://wallpaperaccess.com/full/4487462.jpg" height="800px"/>  
+            
             
        </div>
         
