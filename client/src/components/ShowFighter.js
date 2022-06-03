0570c2e6f3e93ba4a6f5c985e1562301265dca20
Ruslan Khait <rusty6916@gmail.com>
2022-06-03 11:15:28 -0400

    styled show fighter with css

--- client/src/components/ShowFighter.js
+++ client/src/components/ShowFighter.js
@@ -1,20 +1,26 @@
 import React, {useState, useEffect} from 'react'
+import '../showfighter.css'
+
 function ShowFighter ({fighterName,fighterImage, fighterRanking, fighter_record, fighterAge, fighterHeight, fighterWeight, fighterReach,fighterNickName}) {
     
     return(
-    <div>
-        <h1 style={{color: "white", fontSize: "18px"}}>{fighterName}</h1>
-        <h1 style={{color: "white", fontSize: "18px"}}>Nickname: {fighterNickName}</h1>
-        <img src={fighterImage}/>
-        <h2 style={{color: "white", fontSize: "18px"}}>Ranking: { fighterRanking}</h2>
-        <h2 style={{color: "white", fontSize: "12px"}}>Record: {fighter_record}</h2>
-        <h2 style={{color: "white", fontSize: "12px"}}>Age: {fighterAge}</h2>
-        <h2 style={{color: "white", fontSize: "12px"}}>Height: {fighterHeight} in</h2>
-        <h2 style={{color: "white", fontSize: "12px"}}>Weight: {fighterWeight} lbs</h2>
-        <h2 style={{color: "white", fontSize: "12px"}}>Reach: {fighterReach} </h2>
-        <button>Delete</button>
-        <button>Update</button>
-    </div>
+        <div className="show-fighter">
+            <div className="show-fighter-flex">
+                 <img className="show-fighter-img" src={fighterImage}/>
+                 <h2 className="show-fighter-text" style={{color: "white", fontSize: "18px"}}>Ranking: { fighterRanking}</h2>
+                 <h2 className="show-fighter-text">Record: {fighter_record}</h2>
+                 <h2 className="show-fighter-text">Age: {fighterAge}</h2>
+                 <h2 className="show-fighter-text">Height: {fighterHeight} in</h2>
+                 <h2 className="show-fighter-text">Weight: {fighterWeight} lbs</h2>
+                 <h2 className="show-fighter-text">Reach: {fighterReach} </h2>
+            </div>
+            <div className="show-fighter-text-name">
+                <h1 className="show-fighter-text-fullname">{fighterName}</h1>
+                <h1 className="show-fighter-text-nickname">Nickname: {fighterNickName}</h1>
+                <button className="show-fighter-del">Delete</button>
+                <button className="show-fighter-update">Update</button>
+            </div>
+        </div>
     )
 }
 
